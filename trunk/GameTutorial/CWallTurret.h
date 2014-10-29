c8ec18c88e4d177393c5e308cf405f9c89de5bcc
ducsangpro.93@gmail.com <ducsangpro.93@gmail.com@daefc74e-f464-424d-00f8-f4db7ba3c404>
2014-10-29 02:45:01 +0000

    CWallTurret

---
+++ trunk/GameTutorial/CWallTurret.h
@@ -0,0 +1,45 @@
+#ifndef __CWALL_TURRET_H__
+#define __CWALL_TURRET_H__
+
+#include "CStaticObject.h"
+#include "CAnimation.h"
+#include "CBulletN.h"
+
+enum WALLTURRET_SHOOT_STATE
+{
+	W_IS_SHOOTING_DOWN = 0,
+	W_IS_SHOOTING_UP = 1,
+	W_IS_SHOOTING_NORMAL = 2,
+	W_IS_SHOOTING_DIAGONAL_UP_X = 3,
+	W_IS_SHOOTING_DIAGONAL_UP_2X = 4,
+	W_IS_SHOOTING_DIAGONAL_DOWN_X = 5,
+	W_IS_SHOOTING_DIAGONAL_DOWN_2X = 6
+};
+
+class CWallTurret : public CStaticObject, public CAnimation
+{
+public:
+	CWallTurret(void);
+	CWallTurret(const std::vector<int>& info);
+	~CWallTurret();
+	std::string ClassName(){return __CLASS_NAME__(CWallTurret);};
+	virtual void Update(float deltaTime);
+	virtual void Update(float deltaTime, std::hash_map<int, CGameObject*>* listObjectCollision);
+	RECT* GetBound();
+	RECT* GetRectRS();
+	Box GetBox();
+protected:
+	void Init();
+	void BulletUpdate(float deltaTime);
+	WALLTURRET_SHOOT_STATE m_stateCurrent;
+	void SetFrame();
+	bool m_isShoot;
+	//Tham so dung de test
+public:
+	//Phai co mot bien de giu nhung vien dan ma enemy da ban ra
+	std::vector<CBullet_N*> m_listBullet;
+	int m_bulletCount; //So luong vien dan da ban ra
+	float m_timeDelay;
+};
+
+#endif // !__CWALL_TURRET_H__
